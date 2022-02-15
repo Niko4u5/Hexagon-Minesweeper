32d8100a13c1054815075d00ad3aaa71773b9c56
nordlicht28 <yesenia@moehring-family.de>
2022-02-15 14:51:27 +0100

    TODO: create tiles

--- src/World.java
+++ src/World.java
@@ -1,11 +1,10 @@
-import javax.swing.*;
 import java.awt.*;
 
 /**
  * contains all the tiles and manages aces to them.
  */
 public class World {
-    Tile[] tiles;
+    Tile[][] tiles;
     int sizeX, sizeY;
     int numbers; // number of tiles that need to be clicked.
 
@@ -13,9 +12,24 @@ public class World {
     World(int x, int y,MyCanvas window){
         sizeX = x;
         sizeY = y;
-        this.window = window;
-        //TODO: create tiles.
-        //TODO: set numbers.
+        tiles = new Tile[sizeX][sizeY];
+
+        for (int i = 0; i < sizeX; i++){
+            for (int j = 0; j < sizeY; j++){
+                tiles[i][j] = new Number(i,j,window,this);
+            }
+        }
+        int bombs = 5;
+        while (bombs > 0){
+            int i = (int)(Math.random() * sizeX);
+            int j = (int)(Math.random() * sizeY);
+
+            if (tiles[i][j] instanceof Bomb){
+                return;
+            } else {
+                tiles[i][j] = new Bomb(i,j,window,this);
+            }
+        }
     }
 
     public void paint(Graphics g){
