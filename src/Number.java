875761998f06cf1461db729422afa3a6f75e8122
Niko4u5 <niko4u@fam-email.de>
2022-02-18 17:53:55 +0100

    calculating neighboring bombs

--- src/Number.java
+++ src/Number.java
@@ -4,12 +4,12 @@ import java.awt.*;
  * Goal of the Game is to click all of these, on Click they show the number of bombs next to them.
  */
 public class Number extends Tile {
-    int neighbors;
+
+    int posX;
+    int posY;
+
     public Number(int posX, int posY, MyCanvas window, World world) {
         super(posX, posY, window, world);
-
-        number = new TextArea();
-        number.setBounds(hexagon.getBounds());
 
         this.posX = posX;
         this.posY = posY;
@@ -24,9 +24,7 @@ public class Number extends Tile {
                 world.win();
             }
             revealed = true;
-            number.setText(Integer.toString(calculateNeighbors()));
-            number.setVisible(true);
-            number.setSize(10,10);
+
             world.repaint();
         }
 
